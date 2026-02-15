c2c29a1006454ab878d2cd21a381a70cad2b995c
Jorge08lds <jorge08lds@gmail.com>
2026-02-15 18:36:06 -0500

    Complete Ejercice 01

---
+++ LogicConcepts/isNumberOddorNot/Program.cs
@@ -0,0 +1,31 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese número o 'S' para salir:  ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "S")
+    {
+        Console.WriteLine("Saliendo del programa...");
+        break;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt}, es par");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt}, es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El valor {numberString} no es un número válido");
+    }
+
+} while (numberString!.ToLower() != "S");
+
+
+
