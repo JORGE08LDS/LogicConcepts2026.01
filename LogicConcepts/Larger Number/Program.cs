4175efad94fd91dbc0efd02bf17775a81d3cda73
Jorge08lds <jorge08lds@gmail.com>
2026-02-19 01:13:49 -0500

    Larger Number

---
+++ LogicConcepts/Larger Number/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Ingrese el primer número: ");
+double N1 = double.Parse(Console.ReadLine()!);
+Console.Write("Ingrese el segundo número: ");
+double N2 = double.Parse(Console.ReadLine()!);
+Console.Write("Ingrese el tercer número: ");
+double N3 = double.Parse(Console.ReadLine()!);
+
+if (N1 > N2 && N1 > N3)
+{
+    Console.WriteLine($"El numero {N1}, es el mayor");
+}
+else if (N2 > N1 && N2 > N3)
+{
+    Console.WriteLine($"El numero {N2}, es el mayor");
+       
+}
+else
+{
+    Console.WriteLine($"El numero {N3}, es el mayor");
+
+}
---
+++ LogicConcepts/Share/ConsoleExtension.cs
@@ -0,0 +1,6 @@
+﻿namespace Share;
+
+public static class ConsoleExtension
+{
+
+}
