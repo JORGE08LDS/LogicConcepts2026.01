e65d9a15caa881d0ccc176577dc36cc77dd3525b
Jorge08lds <jorge08lds@gmail.com>
2026-02-19 01:17:06 -0500

    Bucle

--- LogicConcepts/Larger Number/Program.cs
+++ LogicConcepts/Larger Number/Program.cs
@@ -1,21 +1,24 @@
-﻿Console.Write("Ingrese el primer número: ");
-double N1 = double.Parse(Console.ReadLine()!);
-Console.Write("Ingrese el segundo número: ");
-double N2 = double.Parse(Console.ReadLine()!);
-Console.Write("Ingrese el tercer número: ");
-double N3 = double.Parse(Console.ReadLine()!);
+﻿do
+{
+    Console.Write("Ingrese el primer número: ");
+    double N1 = double.Parse(Console.ReadLine()!);
+    Console.Write("Ingrese el segundo número: ");
+    double N2 = double.Parse(Console.ReadLine()!);
+    Console.Write("Ingrese el tercer número: ");
+    double N3 = double.Parse(Console.ReadLine()!);
 
-if (N1 > N2 && N1 > N3)
-{
-    Console.WriteLine($"El numero {N1}, es el mayor");
-}
-else if (N2 > N1 && N2 > N3)
-{
-    Console.WriteLine($"El numero {N2}, es el mayor");
-       
-}
-else
-{
-    Console.WriteLine($"El numero {N3}, es el mayor");
+    if (N1 > N2 && N1 > N3)
+    {
+        Console.WriteLine($"El numero {N1}, es el mayor");
+    }
+    else if (N2 > N1 && N2 > N3)
+    {
+        Console.WriteLine($"El numero {N2}, es el mayor");
 
-}
+    }
+    else
+    {
+        Console.WriteLine($"El numero {N3}, es el mayor");
+
+    }
+} while (true);
