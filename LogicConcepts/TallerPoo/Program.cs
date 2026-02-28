e7f7cf6df1c431fa8d5e1fa07cf8a14c8b3bd564
Jorge08lds <jorge08lds@gmail.com>
2026-02-28 01:59:41 -0500

    Taller01

---
+++ LogicConcepts/TallerPoo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Time t1 = new Time();
+            Time t2 = new Time(14);
+            Time t3 = new Time(9, 34);
+            Time t4 = new Time(19, 45, 56);
+            Time t5 = new Time(23, 3, 45, 678);
+
+            Time[] times = { t1, t2, t3, t4, t5 };
+
+            foreach (Time t in times)
+            {
+                Console.WriteLine($"Time: {t}");
+
+                Console.WriteLine($"        {"Milliseconds:",-14}{t.ToMilliseconds(),15:N0}");
+                Console.WriteLine($"        {"Seconds     :",-14}{t.ToSeconds(),15:N0}");
+                Console.WriteLine($"        {"Minutes     :",-14}{t.ToMinutes(),15:N0}");
+
+                Time added = t.Add(t3);
+                Console.WriteLine($"        {"Add         :",-14}{added}");
+
+                Console.WriteLine($"        {"Is Other day:",-14}{t.IsOtherDay(t4)}");
+                Console.WriteLine();
+            }
+
+            // Invalid test
+            Time invalid = new Time(45);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ LogicConcepts/TallerPoo/Time.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+public class Time
+{
+    private int hour;
+    private int minute;
+    private int second;
+    private int millisecond;
+
+    // Constructor 1: no parameters
+    public Time() : this(0, 0, 0, 0) { }
+
+    // Constructor 2: hour
+    public Time(int hour) : this(hour, 0, 0, 0) { }
+
+    // Constructor 3: hour and minute
+    public Time(int hour, int minute) : this(hour, minute, 0, 0) { }
+
+    // Constructor 4: hour, minute and second
+    public Time(int hour, int minute, int second) : this(hour, minute, second, 0) { }
+
+    // Constructor 5: full constructor
+    public Time(int hour, int minute, int second, int millisecond)
+    {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+        this.millisecond = millisecond;
+
+        ValidateTime();
+    }
+
+    private void ValidateTime()
+    {
+        if (hour < 0 || hour > 23)
+            throw new ArgumentException($"The hour: {hour}, is not valid.");
+
+        if (minute < 0 || minute > 59)
+            throw new ArgumentException("Invalid minute");
+
+        if (second < 0 || second > 59)
+            throw new ArgumentException("Invalid second");
+
+        if (millisecond < 0 || millisecond > 999)
+            throw new ArgumentException("Invalid millisecond");
+    }
+
+    public override string ToString()
+    {
+        int displayHour = hour % 12;
+        if (displayHour == 0)
+            displayHour = 12;
+
+        string period = hour < 12 ? "AM" : "PM";
+
+        return $"{displayHour:D2}:{minute:D2}:{second:D2}.{millisecond:D3} {period}";
+    }
+
+    public int ToMilliseconds()
+    {
+        return (((hour * 60 + minute) * 60 + second) * 1000 + millisecond);
+    }
+
+    public int ToSeconds()
+    {
+        return ((hour * 60 + minute) * 60 + second);
+    }
+
+    public int ToMinutes()
+    {
+        return (hour * 60 + minute);
+    }
+
+    public bool IsOtherDay(Time other)
+    {
+        int totalHours = hour + other.hour;
+        int totalMinutes = minute + other.minute;
+        int totalSeconds = second + other.second;
+        int totalMilliseconds = millisecond + other.millisecond;
+
+        if (totalMilliseconds > 999) totalSeconds++;
+        if (totalSeconds > 59) totalMinutes++;
+        if (totalMinutes > 59) totalHours++;
+
+        return totalHours > 23;
+    }
+
+    public Time Add(Time other)
+    {
+        int ms = millisecond + other.millisecond;
+        int sec = second + other.second;
+        int min = minute + other.minute;
+        int hr = hour + other.hour;
+
+        if (ms > 999)
+        {
+            sec++;
+            ms -= 1000;
+        }
+
+        if (sec > 59)
+        {
+            min++;
+            sec -= 60;
+        }
+
+        if (min > 59)
+        {
+            hr++;
+            min -= 60;
+        }
+
+        if (hr > 23)
+        {
+            hr -= 24;
+        }
+
+        return new Time(hr, min, sec, ms);
+    }
+}
